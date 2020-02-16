51d94ac1c38faf202dd48166e1d17be044d25a5c
andrew-ashbaugh <andrew1552@hotmail.com>
2020-02-16 04:50:44 -0500

    First Commit
    Welcome Shandrew Ashfloy

---
+++ ParkourParkour/Assets/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float horizontal;
+    public float vertical;
+    public float walkSpeed;
+    public float mouseSensitivity;
+    private float speed;
+    private Rigidbody rb;
+    public int jumpForce;
+    public Transform camera1;
+    public LayerMask groundLayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        bool grounded = (Physics.Raycast(transform.position, Vector3.down, 1f, 1 << LayerMask.NameToLayer("Ground"))); // Checks for ground beneeth you
+        horizontal = Input.GetAxis("Horizontal"); // horizontal input
+        vertical = Input.GetAxis("Vertical"); // vertical input
+        rb.MovePosition(transform.position + (transform.forward * vertical * speed) + (transform.right * horizontal * speed)); // move the player
+        
+      
+
+        if (Input.GetKey(KeyCode.LeftShift) && grounded == true) // sprinting
+        {
+            speed = walkSpeed * 2;
+        }
+        else
+        {
+            speed = walkSpeed;
+        }
+
+
+        if (grounded == true && Input.GetButtonDown("Fire1")) // jumping
+        {
+            rb.AddForce(Vector3.up * jumpForce);
+        }
+        // rb.velocity += transform.forward * vertical * speed;
+        // rb.velocity += transform.right * horizontal * speed;
+     
+        
+        transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime); // mouse x movement
+        camera1.transform.Rotate(-Vector3.right * Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime); // mouse y movement
+
+       
+
+    }
+}
---
+++ ParkourParkour/Assets/Scripts/Generator.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+    [Header("Place your building prefabs in here")]
+    public GameObject[] buildings;
+
+    [Header("How far apart do you want them?")]
+    [Space(25)]
+    public float distanceApart;
+
+    [Header("How many buildings do you want?")]
+    [Space(25)]
+
+    public int amount;
+    private List<GameObject> buildingsPlaced = new List<GameObject>();
+    private bool canPlace;
+    private Vector3 tempPos;
+    private Vector3 bestPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject buildingsSpawnedParent = new GameObject("BuildingSeed");
+        buildingsSpawnedParent.transform.position = Vector3.zero;
+        //buildingsPlaced = new List<GameObject>(amount);
+        for (int i = 0; i < amount; i++)
+        {
+            while(canPlace == false)
+            {
+
+                if(buildingsPlaced.Count == 0) // if placing 1st building put it at 000
+                {
+                    GameObject tempBuilding = (GameObject)Instantiate(buildings[Random.Range(0, buildings.Length)], new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+                    tempBuilding.transform.parent = buildingsSpawnedParent.transform;
+                    tempBuilding.transform.localRotation = Quaternion.Euler(-90, 0, 0);
+                    buildingsPlaced.Add(tempBuilding);
+                    canPlace = true;
+                }
+                else if(buildingsPlaced.Count>=1)// if placing 2nd building
+                {
+                    for (int  j = 0;  j <buildingsPlaced.Count;  j++) // cycle through buildings
+                    {
+                        
+                            tempPos = Random.insideUnitSphere * distanceApart + buildingsPlaced[j].transform.position;
+                           // Debug.Log(tempPos);
+                        
+                        
+                        if(Vector3.Distance(tempPos, buildingsPlaced[j].transform.position) > distanceApart/2) // if distance is good, this is our best pos
+                        {  
+                                bestPos = tempPos;
+                               // Debug.Log(bestPos);
+                            
+                        }
+
+                    }
+                    if(bestPos!= Vector3.zero)
+                    {
+                        GameObject tempBuilding = (GameObject)Instantiate(buildings[Random.Range(0, buildings.Length)], new Vector3(bestPos.x, 0, bestPos.z), Quaternion.identity);
+                        tempBuilding.transform.parent = buildingsSpawnedParent.transform;
+                        tempBuilding.transform.localRotation = Quaternion.Euler(-90, 0, 0);
+                        buildingsPlaced.Add(tempBuilding);
+                        
+                        canPlace = true;
+                        bestPos = Vector3.zero;
+                    }
+                    
+
+                }
+               
+            }
+            canPlace = false;
+
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+}
